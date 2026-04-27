f75ea161ff7c71031d792417b77dfd05dad8f610
Lars Krönke <155492086+LarsKroenke@users.noreply.github.com>
2026-04-27 11:13:18 +0200

    Refactor emission.py for improved spectrum analysis
    Refactor emission.py to enhance functionality and improve layout. Added new features for spectrum selection and plotting integration.

--- emission.py
+++ emission.py
@@ -1,43 +1,93 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+import requests as re
+import numpy as np
 
-# table part
-
-
-def ex2df(file):
-    return pd.read_csv(file, sep='\t', engine='python')
-
-
-def concat_df_7(probe):
-    file_1 = f'2k-klebstoffe/component-tables/7-days/{probe}_1.txt'
-    file_2 = f'2k-klebstoffe/component-tables/7-days/{probe}_2.txt'
-    file_3 = f'2k-klebstoffe/component-tables/7-days/{probe}_3.txt'
-
-    df_1 = ex2df(file_1)
-    df_2 = ex2df(file_2)
-    try:
-        df_3 = ex2df(file_3)
-    except FileNotFoundError:
-        df_3 = pd.DataFrame()
-
-    return pd.concat([df_1, df_2, df_3], ignore_index=True)
+st.set_page_config(
+    page_title="Full Width App",
+    page_icon="🖥️",
+    layout="wide",  # Enables full-width layout
+    initial_sidebar_state="expanded"
+)
 
 
-def concat_df_28(probe):
-    file_1 = f'2k-klebstoffe/component-tables/28-days/{probe}_1.txt'
-    file_2 = f'2k-klebstoffe/component-tables/28-days/{probe}_2.txt'
-    file_3 = f'2k-klebstoffe/component-tables/28-days/{probe}_3.txt'
+def main():
+    option = st.sidebar.selectbox(
+        'Analyten auswählen',
+        ('pn7444', 'lord', '9075l', 'ep6055', 'dp6330')
+    )
 
-    df_1 = ex2df(file_1)
-    df_2 = ex2df(file_2)
-    df_3 = ex2df(file_3)
+    option_hit = st.sidebar.selectbox(
+        'Hit der Spektrenbibliothek auswählen', (1, 2, 3))
 
-    return pd.concat([df_1, df_2, df_3], ignore_index=True)
+    hit_argument = ['Peak Name', 'Peak Name Hit 2', 'Peak Name Hit 3']
+    df_summary = read_file(option, '7')
+    df_stat = prepare_data(df_summary, hit_argument[option_hit-1])
+
+    df_summary_28 = read_file(option, '28')
+    df_stat_28 = prepare_data(df_summary_28, hit_argument[option_hit-1])
+
+    # plot integration
+    x, y, x_28, y_28 = read_raw_spectra(option)
+
+    st.sidebar.write('Spektrum ein- ausblenden')
+    on_7 = st.sidebar.toggle('7 Tage', value=True)
+    on_28 = st.sidebar.toggle('28 Tage', value=False)
+
+    st.sidebar.write('Toggle Annotations')
+    annotation = st.sidebar.toggle('Annotation', value=True)
+
+    slider = st.sidebar.slider(
+        'Retentionszeitbereich (min)',
+        0, 60, (0, 60)
+    )
+    if on_28:
+        slider_y = st.sidebar.slider(
+            'Intensitätsbereich',
+            0.0, max(max(y), max(y_28)), (0.0, max(max(y), max(y_28)))
+        )
+    else:
+        slider_y = st.sidebar.slider(
+            'Intensitätsbereich',
+            0.0, max(y), (0.0, max(y))
+        )
+
+    st.sidebar.write('Chromatogramm offset justieren')
+    x_offset = st.sidebar.slider('X offset', 0.0, 2.0, step=0.05, value=0.0)
+    y_offset = st.sidebar.slider(
+        'Y offset', 0.0*10**9, 0.5*10**9, step=0.01*10**9, value=0.0)
+
+    plot = plot_spectra(x, y, x_28, y_28, on_7, on_28,
+                        slider, slider_y, df_stat, x_offset, y_offset, annotation)
+
+    # Inhalte schreiben
+    # st.header('Auswertung der Dreifachbestimmung')
+    st.write(plot)
+    st.write(
+        '7 Tage Messung (gruppiert nach Retentionszeit und übereinstimmendem Hit)')
+    st.write(df_stat)
+    st.write(
+        '28 Tage Messung (gruppiert nach Retentionszeit und übereinstimmendem Hit)')
+    st.write(df_stat_28)
 
 
-def group_dfs(df_all):
+def read_file(probe, days):
+    base_path = f'2k-klebstoffe/component-tables/{days}-days'
+    dfs = []
+
+    for i in range(1, 4):
+        try:
+            df = pd.read_csv(f"{base_path}/{probe}_{i}.txt",
+                             sep='\t', engine='python')
+            dfs.append(df)
+        except FileNotFoundError:
+            continue
+
+    return pd.concat(dfs, ignore_index=True) if dfs else pd.DataFrame()
+
+
+def prepare_data(df_all, hit):
 
     df_all["Retentionszeit"] = (df_all["Retention Time"].astype(str).str.replace(
         ',', '.', regex=False).pipe(pd.to_numeric, errors='coerce') / 0.11).round() * 0.11
@@ -60,7 +110,7 @@ def group_dfs(df_all):
 
     summary = (
         df_all
-        .groupby(["Peak Name", "Retentionszeit"], dropna=False)
+        .groupby([hit, "Retentionszeit"], dropna=False)
         .agg(
             n=("Amount", "count"),
             mean=("Amount", "mean"),
@@ -69,16 +119,14 @@ def group_dfs(df_all):
             P=("Prob", "mean"),
             RSI=("RSI ", "mean")
         )
-        .reset_index()
     )
     summary[["mean", "td8", "sx", "P", "RSI"]] = summary[[
         "mean", "td8", "sx", "P", "RSI"]].round(1)
     summary.rename(columns={'Peak Name': 'Komponente', 'Retentionszeit': 'Retentionszeit', 'mean': 'Toluol Äq.',
                    'sx': 'Standardabweichung', 'P': 'Probability', 'td8': 'int. Std. Äq.'}, inplace=True)
-    return summary.sort_values('Retentionszeit')
+    return summary.sort_values('Retentionszeit').reset_index()
 
 
-# plot the data
 def chrom2df(file_path):
     # Read file
     with open(file_path, "r", encoding="utf-8") as f:
@@ -113,78 +161,95 @@ def chrom2df(file_path):
     return df
 
 
-def read_plot_data_7(kleber):
+def read_raw_spectra(kleber):
     file_7 = f'2k-klebstoffe/spectra/7-days/{kleber}.txt'
-    return chrom2df(file_7)
+    df_7 = chrom2df(file_7)
 
-
-def read_plot_data_28(kleber):
     file_28 = f'2k-klebstoffe/spectra/28-days/{kleber}.txt'
-    return chrom2df(file_28)
+    df_28 = chrom2df(file_28)
 
-
-def plot_spectra(df_7, df_28, slider, on_7, on_28, x_offset, y_offset):
     x_7 = df_7["Time"]
     y_7 = df_7["Value"]
-    x_28 = df_28["Time"] + x_offset
-    y_28 = df_28["Value"] - y_offset
+    x_28 = df_28["Time"]  # + x_offset
+    y_28 = df_28["Value"]  # - y_offset
+
+    return x_7, y_7, x_28, y_28
+
+
+def plot_spectra(x_7, y_7, x_28, y_28, on_7, on_28, slider, slider_y, df_stat, x_offset, y_offset, annotate):
+    from scipy.interpolate import interp1d
 
     fig, ax = plt.subplots(figsize=(10, 4))
     if on_7:
         ax.plot(x_7, y_7, linewidth=0.25, color='black', label='7 Tage')
     if on_28:
-        ax.plot(x_28, y_28, linewidth=0.25, color='red', label='28 Tage')
+        ax.plot(x_28 + x_offset, y_28 + y_offset,
+                linewidth=0.25, color='red', label='28 Tage')
     ax.set_xlabel('Retentionszeit in min')
     ax.set_ylabel('Intensität')
     ax.legend()
-    # ax.spines.right.set_color(None)
-    # ax.spines.top.set_color(None)
-    # ax.spines.bottom.set_bounds(slider)
-    # ax.spines.left.set_bounds(0, 5.5*10**9)
-    ax.set_xticks(np.arange(0, 60, 1), minor=True)
-    ax.set_xticks(np.arange(0, 61, 5))
+    ax.spines.right.set_color(None)
+    ax.spines.top.set_color(None)
+    ax.spines.bottom.set_bounds(slider)
+    ax.spines.left.set_bounds(min(slider_y), max(slider_y))
+    ax.set_xticks(np.arange(min(slider), max(slider), 1), minor=True)
+    ax.set_xticks(np.arange(min(slider), max(slider)+1, 5))
     ax.set_xlim(slider)
+    ax.set_ylim(min(slider_y)-(max(slider_y)/25), max(slider_y))
+
+    # df_measurement_1
+    # df_stat["Retention Time"] = (df_stat["Retention Time"].astype(str).str.replace(
+    # ',', '.', regex=False).pipe(pd.to_numeric, errors='coerce'))
+
+    # df_stat
+    df_stat["Retentionszeit"] = (df_stat["Retentionszeit"].astype(str).str.replace(
+        ',', '.', regex=False).pipe(pd.to_numeric, errors='coerce'))
+
+    # for i in range(len(df_stat)):
+    #     target_time = df_stat.iloc[i, 2]
+    #     # intensity = np.interp(target_time, x_7, y_7)
+
+    #     f = interp1d(x_7, y_7, kind='cubic', fill_value="extrapolate")
+    #     intensity = f(target_time)
+    #     ax.annotate(f'{i}', xy=(target_time, intensity), fontsize=6)
+    #     num += 1
+
+    if annotate & on_7:
+        window = 0.05  # adjust
+
+        x = np.asarray(x_7)
+        y = np.asarray(y_7)
+
+        for i in range(len(df_stat)):
+            target_time = df_stat.iloc[i, 1]
+
+            # mask safely
+            mask = (x >= target_time - window) & (x <= target_time + window)
+
+            if not np.any(mask):
+                continue  # skip if nothing found
+
+            local_x = x[mask]
+            local_y = y[mask]
+
+            if len(local_y) == 0:
+                continue
+
+            idx = np.argmax(local_y)
+
+            peak_time = local_x[idx]
+            peak_intensity = local_y[idx]
+
+            ax.annotate(
+                f'{i}',
+                xy=(peak_time, peak_intensity),
+                xytext=(0, 5),
+                textcoords='offset points',
+                ha='center',
+                fontsize=5
+            )
     return fig
 
 
-# streamlit code
-option = st.sidebar.selectbox(
-    'Analyten auswählen',
-    ('pn7444', 'lord', '9075l', 'ep6055', 'dp6330')
-)
-
-# plot integration
-slider = st.sidebar.slider(
-    'Retentionszeitbereich (min)',
-    0, 60, (0, 60)
-)
-
-st.sidebar.write('Spektrum ein- ausblenden')
-on_7 = st.sidebar.toggle('7 Tage', value=True)
-on_28 = st.sidebar.toggle('28 Tage', value=True)
-
-st.sidebar.write('Chromatogramm offset justieren')
-x_offset = st.sidebar.slider('X offset', 0.0, 2.0, step=0.05, value=0.5)
-y_offset = st.sidebar.slider(
-    'Y offset', 0.0*10**9, 0.5*10**9, step=0.01*10**9, value=0.1*10**9)
-
-df_7 = read_plot_data_7(option)
-df_28 = read_plot_data_28(option)
-
-plot = plot_spectra(df_7, df_28, slider, on_7, on_28, x_offset, y_offset)
-
-st.write(plot)
-
-# 7 days calculations
-df_all_7 = concat_df_7(option)
-component_table_7 = group_dfs(df_all_7)
-
-# 28 days calculations
-df_all_28 = concat_df_28(option)
-component_table_28 = group_dfs(df_all_28)
-
-st.write('Komponententabelle (7 Tage)')
-st.write(component_table_7)
-
-st.write('Komponententabelle (28 Tage)')
-st.write(component_table_28)
+if __name__ == "__main__":
+    main()
