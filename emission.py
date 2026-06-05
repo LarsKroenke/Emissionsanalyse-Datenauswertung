6aa33a73c30bd0b23578323fb52dfba272cb63d9
Lars Krönke <155492086+LarsKroenke@users.noreply.github.com>
2026-06-05 12:20:51 +0200

    Fix aggregation calculation for mean in emission.py

--- emission.py
+++ emission.py
@@ -133,7 +133,7 @@ def prepare_data(df_all, hit):
         .groupby([hit, "Retentionszeit"], dropna=False)
         .agg(
             n=("Amount", "count"),
-            mean=("Amount", "mean") / "Amount",
+            mean=("Amount", "mean"),
             td8=("Est. Amt.", "mean"),
             sx=("Amount", "std"),
             P=("Prob", "mean"),
