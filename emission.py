6099745d2a9a0cebe6db6c7872626193576ce59e
Lars Krönke <155492086+LarsKroenke@users.noreply.github.com>
2026-06-06 15:48:16 +0200

    Adjust mean calculation in emission.py

--- emission.py
+++ emission.py
@@ -133,7 +133,7 @@ def prepare_data(df_all, hit):
         .groupby([hit, "Retentionszeit"], dropna=False)
         .agg(
             n=("Amount", "count"),
-            mean=("Amount", "mean"),
+            mean=("Amount", "mean") + 1000,
             td8=("Est. Amt.", "mean"),
             sx=("Amount", "std"),
             P=("Prob", "mean"),
